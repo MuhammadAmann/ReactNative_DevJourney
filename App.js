508bfbdc402713f99ceb0e7a7bc5ded94f091e6f
Muhammad Aman <109753345+MuhammadAmann@users.noreply.github.com>
2024-02-27 01:18:58 +0500

    Dev Journey
    Explore the fascinating journey of React Native development! This repository is a collection of practical React Native components, showcasing step-by-step implementations of common functionalities. Ideal for learners and developers seeking hands-on examples, dive into the React Native development journey and enhance your skills. 🚀

---
+++ App.js
@@ -0,0 +1,13 @@
+import {View, Text} from 'react-native';
+import React from 'react';
+
+const App = () => {
+  return (
+    <View>
+      <Text>React Native </Text>
+      <Text>Dev Journey </Text>
+    </View>
+  );
+};
+
+export default App;
